e3922e2b0d27da95f199cc2775e9986bb401db36
Thiago Carvalho <carvalho.tpc@gmail.com>
2025-07-29 17:08:07 -0300

    exercicio 57

--- src/exercicio57/Contador.java
+++ src/exercicio57/Contador.java
@@ -1,6 +1,21 @@
 package exercicio57;
 
-
 public class Contador {
+	private int numero;
 
+	public Contador() {
+		this.numero = 1;
+	}
+	
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public void acrescentar() {
+		this.numero++;
+	}
 }
--- src/exercicio57/Main.java
+++ src/exercicio57/Main.java
@@ -1,6 +1,13 @@
 package exercicio57;
 
-
 public class Main {
 
+	public static void main(String[] args) {
+		Contador numero = new Contador();
+		
+		do {
+			System.out.println(numero.getNumero());
+			numero.acrescentar();
+		} while(numero.getNumero() <= 10);
+	}
 }
