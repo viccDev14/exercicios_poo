46cc3e8f0883378501c06fdd18775b3b9c767eac
viccDev14 <vicsignos@gmail.com>
2025-07-30 17:41:07 -0300

    atualizado

--- src/exercicio58/Main.java
+++ src/exercicio58/Main.java
@@ -5,9 +5,8 @@ public class Main {
 		Contador numero = new Contador();
 
 		while (numero.getNumero() >= 1) {
-			numero.retirar();
 			System.out.println(numero.getNumero());
-
+			numero.retirar();
 		}
 	}
 }
--- src/exercicio59/Main.java
+++ src/exercicio59/Main.java
@@ -1,5 +1,7 @@
 package exercicio59;
 
 public class Main {
-
+	public static void main(String[] args) {
+		
+	}
 }
